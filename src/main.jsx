9699d33b91bd001ce3f10546eea7787d45d5b523
CYNTAX <samuelify225@gmail.com>
2024-06-13 17:53:07 +0100

    update

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,9 @@ import App from "./App.jsx";
 import "./index.css";
 
 window.Telegram.WebApp.expand();
+window.Telegram.WebApp.BackButton.setCallback(async () => {
+  // This code will be executed when the user tries to close the mini app
+});
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
