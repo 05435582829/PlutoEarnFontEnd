43038fd0a8d441c54e6ecad33be44462a4f95985
kinglsey <goodluckcanhelp@gmail.com>
2024-06-14 11:31:23 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,27 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 import Home from "./Home/Home";
 
 function App() {
+  const [userInfo, setUserInfo] = useState(null);
+
+  useEffect(() => {
+    const initTelegramWebApp = async () => {
+      if (window.Telegram && window.Telegram.WebApp) {
+        try {
+          const userInfo = await window.Telegram.WebApp.getUserInfo();
+          setUserInfo(userInfo);
+        } catch (error) {
+          console.error("Error getting user info:", error);
+        }
+      }
+    };
+
+    initTelegramWebApp();
+  }, []);
   return (
     <div className="App">
-      <Home />
+      {userInfo ? <Home /> : <p> THis is not loaded inside a telegram app</p>}
     </div>
   );
 }
