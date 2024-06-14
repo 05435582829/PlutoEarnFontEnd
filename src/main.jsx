1916804d093e1845b628e60a2a9ce60b14c77707
kinglsey <goodluckcanhelp@gmail.com>
2024-06-14 13:15:59 +0100

    update

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,9 @@ import { UserProvider } from "./Utils/UserContext.jsx";
 
 //make telegram webapp view show site fullscreen
 window.Telegram.WebApp.expand();
-window.Telegram.WebApp.BackButton.setCallback(async () => {});
+window.Telegram.WebApp.BackButton.setCallback(async () => {
+  console.log("hell");
+});
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <UserProvider>
