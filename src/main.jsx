60f24224166c51e7bb225f1159fb275bc06b9b75
kinglsey <goodluckcanhelp@gmail.com>
2024-06-14 13:10:55 +0100

    update

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import "./index.css";
 import { UserProvider } from "./Utils/UserContext.jsx";
 
 //make telegram webapp view show site fullscreen
-// window.Telegram.WebApp.expand();
+window.Telegram.WebApp.expand();
 // window.Telegram.WebApp.BackButton.setCallback(async () => {});
 
 ReactDOM.createRoot(document.getElementById("root")).render(
