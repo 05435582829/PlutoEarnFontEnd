e407fab5f20c07eb43efb9df9f29f499c5c646e0
CYNTAX <samuelify225@gmail.com>
2024-06-14 10:56:18 +0100

    update

--- src/constants/api.js
+++ src/constants/api.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
 export const SignupLogin = async () => {
-  console.log(payload);
   try {
     const res = await axios.get(
       `https://plutoearn.egoras.com/pub/user/register/login/Ebrix/Ebri239`
